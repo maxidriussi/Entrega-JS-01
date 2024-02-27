9d3841f93f8e3f480bfa5999025138b59b031281
maxidriussi <maxiargen98@gmail.com>
2024-02-27 19:02:38 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,104 @@
+///////////////Ejercicio 1///////////////
+
+// function parOImpar (numero){
+//     if(numero % 2===0) {
+//         console.log("El numero es par.");
+//     }else {
+//         console.log("El numero es impar.");
+//     }
+// }
+
+// let numero=prompt("Ingrese su numero.");
+// parOImpar(numero);
+
+///////////////Ejercicio 2///////////////
+
+// function mayorOIgual(numero1, numero2) {
+//   if (numero1 > numero2) {
+//     console.log(numero1 + " es mayor que " + numero2);
+//   } else if (numero2 > numero1) {
+//     console.log(numero2 + " es mayor que " + numero1);
+//   } else {
+//     console.log("Los numeros son iguales.");
+//   }
+// }
+
+// let numero1 = prompt("Ingrese su numero");
+// let numero2 = prompt("Ingrese su numero");
+
+// mayorOIgual(numero1, numero2);
+
+///////////////Ejercicio 3///////////////
+
+// function multiploDe5(numero){
+//     if(numero % 5===0){
+//         console.log(numero + " si es multiplo de 5");
+//     }else{
+//         console.log(numero + " no es multiplo de 5");
+//     }
+// }
+
+// numero=prompt("Ingrese un numero.");
+// multiploDe5(numero);
+
+///////////////Ejercicio 4///////////////
+
+// function desde0Hasta(numero){
+//     for(let i=0;i<=numero;i++){
+//         console.log(i);
+//     }
+// }
+
+// let numero=prompt("Ingrese un numero.")
+// desde0Hasta(numero);
+
+///////////////Ejercicio 5///////////////
+
+// function imprimirPalabra(palabra,cantidad){
+//     for(let i = 0; i < cantidad; i++){
+//         console.log(palabra);
+//     }
+// }
+
+// let palabra = "Labrador";
+// let cantidad = 12;
+
+// imprimirPalabra(palabra,cantidad);
+
+///////////////Ejercicio 6///////////////
+
+// function array(verduras){
+//     for(let i = 0; i < verduras.length; i++){
+//         console.log(verduras[i]);
+//     }
+// }
+
+// let verduras = ["lechuga","cebolla","pimiento","brocoli","papa","batata"];
+
+// array(verduras);
+
+///////////////Ejercicio 7///////////////
+
+// function arraySinElCinco(numeros){
+//     for(let i = 0; i < numeros.length; i++){
+//         if(i !== 4){
+//             console.log(numeros[i]);
+//         }
+//     }
+// }
+
+// let numeros = [1,2,3,4,5,6,7,8,9,10];
+// arraySinElCinco(numeros);
+
+///////////////Ejercicio 8///////////////
+
+// function arrayPorNumero(array,multiplicador){
+//     for(let i = 0; i < array.length; i++){
+//         console.log(array[i] * multiplicador);
+//     }
+// }
+
+// let array = [10,20,30,40,50];
+// let multiplicador = 2;
+
+// arrayPorNumero(array,multiplicador);
